4dc37b907a637010ddb4023efe1064281c5b7e90
Edward Gilbert <egbot@asu.edu>
2024-02-06 13:45:43 -0700

    Spelling update

--- includes/header.php
+++ includes/header.php
@@ -74,7 +74,7 @@
 						<li><a href="<?php echo $CLIENT_ROOT; ?>/projects/index.php?pid=18" >Algae</a></li>
 						<li><a href="<?php echo $CLIENT_ROOT; ?>/projects/index.php?pid=1" >Amphibians</a></li>
 						<li><a href="<?php echo $CLIENT_ROOT; ?>/projects/index.php?pid=3" >Birds</a></li>
-						<li><a href="<?php echo $CLIENT_ROOT; ?>/projects/index.php?pid=4" >Fish</a></li>
+						<li><a href="<?php echo $CLIENT_ROOT; ?>/projects/index.php?pid=4" >Fishes</a></li>
 						<li><a href="<?php echo $CLIENT_ROOT; ?>/projects/index.php?pid=12">Insects &amp; Arachnids</a></li>
 						<li><a href="<?php echo $CLIENT_ROOT; ?>/projects/index.php?pid=11">Mammals</a></li>
 						<li><a href="https://invertebase.org/stri/projects/index.php?pid=4" target="_blank">Marine Invertebrates (InvertEBase)</a></li>
