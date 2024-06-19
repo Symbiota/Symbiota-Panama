8c41ded47bf6982032be7f2fff337e7b6679bcbe
Edward Gilbert <egbot@asu.edu>
2024-06-19 19:39:12 -0400

    header menu adjustments

--- includes/head.php
+++ includes/head.php
@@ -1,4 +1,4 @@
 
 <meta name="viewport" content="initial-scale=1.0, user-scalable=yes" />
 <link href="<?php echo $CLIENT_ROOT; ?>/css/base.css?ver=1" type="text/css" rel="stylesheet" />
-<link href="<?php echo $CLIENT_ROOT; ?>/css/main.css?ver=6" type="text/css" rel="stylesheet" />
+<link href="<?php echo $CLIENT_ROOT; ?>/css/main.css?ver=7" type="text/css" rel="stylesheet" />
